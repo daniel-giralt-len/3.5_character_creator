dc2bedf848ac0f5232da2b67f90dfd24133d10f1
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-05-23 22:12:35 +0200

    add id to db

---
+++ scripts/csv_ider.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+const populateId = path => {
+    const raw = fs.readFileSync(path)
+    const lines = raw.toString().split('\r\n')
+    const [header, ...values] = lines
+
+    if(header.includes('id,')){ return }
+
+    const idValues = values.map((l,i) => `${i+1},${l}`)
+    const rawOut = [header, ...idValues].join('\n')
+
+    fs.writeFileSync(path, rawOut)
+
+}
+
+[
+    ['classes', './db/csv/classes.csv'],
+    ['editions', './db/csv/editions.csv'],
+    ['feats', './db/csv/feats.csv'],
+    ['language', './db/csv/language.csv'],
+    ['races', './db/csv/races.csv'],
+    ['rulebooks', './db/csv/rulebooks.csv'],
+    ['skillTricks', './db/csv/skillTricks.csv'],
+].forEach(([name, path]) => populateId(path))
