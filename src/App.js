fb1018b2776f185588fb9c4d5dfbcedb767839d8
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-05-25 18:14:01 +0200

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import ItemBrowser from './ItemBrowser'
 import dbs from './dbs.json'
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div>
-      <ItemBrowser items={dbs.classes}/>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Races</Link>
+          <Link to="classes">Classes</Link>
+          <Link to="feats">Feats</Link>
+          <Link to="skilltricks">Skilltricks</Link>
+          <Link to="language">Language</Link>
+        </nav>
+        <Outlet />
+        <Routes>
+          <Route path="/" element={<ItemBrowser items={dbs.races} />} />
+          <Route path="classes" element={<ItemBrowser items={dbs.classes} />} />
+          <Route path="feats" element={<ItemBrowser items={dbs.feats} />} />
+          <Route path="skilltricks" element={<ItemBrowser items={dbs.skilltricks} />} />
+          <Route path="language" element={<ItemBrowser items={dbs.language} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
