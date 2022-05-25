caf8aa779a16bb513d97eb69714c71becaafc777
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-05-25 18:01:49 +0200

    implement search bar

--- src/ItemBrowser.js
+++ src/ItemBrowser.js
@@ -1,16 +1,37 @@
+import React, { useState } from 'react';
 
-function renderItems(items) {
-    return items.map(item => (
-        <div>{item.name}</div>
-    ))
+function renderItems(items, searchedText) {
+    const accents = [
+        [/[aàáäâ]/g, 'a'],
+        [/[eèéëê]/g, 'e'],
+        [/[iìíïî]/g, 'i'],
+        [/[oòóöô]/g, 'o'],
+        [/[uùúüû]/g, 'u'],
+        [/[nñ]/g, 'n'],
+        [/ +/g, ' '],
+    ]
+    let regexText = searchedText.toLocaleLowerCase()
+    accents.forEach(accent => regexText = regexText.replace(accent[0], accent[1]))
+    const regex = new RegExp(regexText)
+
+    return items
+        .filter(item => item.name.toLocaleLowerCase().match(regex))
+        .map(item => (<div key={item.id}>{item.name}</div>))
+}
+
+function ItemSearchBar({onChange}){
+    return (<textarea onChange={onChange}></textarea>)
 }
 
 function ItemBrowser({items}) {
-  return (
-    <div>
-      {renderItems(items)}
-    </div>
-  );
+    const [searchedText, setSearchedText] = useState('')
+    const handleSearchChange = event => setSearchedText(event.target.value || '')
+    return (
+        <div>
+            <ItemSearchBar onChange={handleSearchChange} />
+            {renderItems(items, searchedText)}
+        </div>
+    );
 }
 
 export default ItemBrowser;
