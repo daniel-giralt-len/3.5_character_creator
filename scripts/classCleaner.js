0022aae43d51f6cceb34f7c2cd1cda472d091346
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-05-27 23:38:27 +0200

    compile class sections

--- scripts/classCleaner.js
+++ scripts/classCleaner.js
@@ -19,3 +19,25 @@ if(a===0){ //unify all htmls into one dirty json
     }
     fs.writeFileSync(jsonPath, JSON.stringify(json,null,2))
 }
+
+if(a===0){ // figure out some sections
+    const json = require('.'+jsonPath)
+    const onlyUnique = (value, index, self) => self.indexOf(value) === index;
+    const headerRegex = RegExp(/<h[0-9]>(.*?)<\/h[0-9]>/g)
+    const sectionNames = Object
+        .values(json)
+        .map(v=>v.html)
+        .reduce((acc,html) => {
+            return [
+                ...acc,
+                ...(
+                    html
+                        .match(headerRegex)
+                        .map(l => (headerRegex.exec(l) || ['',''])[1].toLowerCase())
+                ||[])
+            ]
+        },[])
+        .filter(onlyUnique)
+    
+    fs.writeFileSync('./src/db/json/itemData/classSections.json', JSON.stringify(sectionNames,null,2))  
+}
