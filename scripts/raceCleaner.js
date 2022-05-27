a03756598a32d6735458ac492706a2d54e0172cf
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-05-27 16:59:28 +0200

    parse race stats

--- scripts/raceCleaner.js
+++ scripts/raceCleaner.js
@@ -6,7 +6,7 @@ const statsRegex = new RegExp('<table class="race-attributes">(.*?)</table>')
 const descriptionRegex = new RegExp('<div class="nice-textile">(.*?)</div>')
 const jsonPath = './src/db/json/itemData/raceDescription.json'
 
-const a = 1
+const a = 2
 
 if(a===0){ //unify all htmls into one dirty json
     const json = {}
@@ -24,7 +24,7 @@ if(a===0){ //unify all htmls into one dirty json
     fs.writeFileSync(jsonPath, JSON.stringify(json,null,2))
 }
 
-if(a===1){ //clean stats
+if(a===1){ //key stats
     const json = require(`.${jsonPath}`)
     const newJson = Object
         .entries(json)
@@ -52,5 +52,67 @@ if(a===1){ //clean stats
         },
         {}
     )
+    fs.writeFileSync(jsonPath, JSON.stringify(newJson,null,2))
+}
+
+if(a===2){ //parse stats
+    const json = require(`.${jsonPath}`)
+
+    const parseNumber = n => parseInt(n.replace('&minus;', '-'))
+    const parseLength = n => parseInt(n.replace('feet', ''))
+    const parseAnchors = s => {
+        const r = new RegExp(/<a href="(?:.*?)">(.*?)<\/a>/g)
+        let links = []
+        let match
+        while ((match = r.exec(s)) !== null) { links.push(match[1]) }
+        return links
+    }
+
+    const parser = {
+        'description': {key: 'description', parse: s=>s },
+        'Size': {key: 'size', parse: s => s.toLowerCase() },
+        'Base speed': {key: 'base speed', parse: s=>s },
+        'Strength': {key: 'STR', parse: parseNumber },
+        'Dexterity': {key: 'DEX', parse: parseNumber },
+        'Constitution': {key: 'CON', parse: parseNumber },
+        'Intelligence': {key: 'INT', parse: parseNumber },
+        'Wisdom': {key: 'WIS', parse: parseNumber },
+        'Charisma': {key: 'CHA', parse: parseNumber },
+        'Reach': {key: 'reach', parse: parseLength },
+        'Bonus Languages': {key: 'bonus languages', parse: parseAnchors },
+        'Racial Base Attack': {key: 'racial base attack', parse: parseNumber },
+        'Racial Base Saves': {key: 'racial base saves', parse: s => {
+            const r = RegExp(/Fort (\+[0-9]+),Reflex (\+[0-9]+),Will (\+[0-9]+)/)
+            const [ _ , fortitude, reflex, will] = r.exec(s)
+            return { fortitude, reflex, will }
+        } },
+        'Natural armor': {key: 'natural armor', parse: parseNumber },
+    }  
+
+    const newJson = Object
+        .entries(json)
+        .reduce((acc, [id, o]) => {
+            console.log(id)
+            const newO = Object
+                .entries(o)
+                .reduce((acc2, [name, value]) => {
+                    console.log(name)
+                    return {
+                        ...acc2,
+                        [parser[name].key]: parser[name].parse(value)
+                    }
+                },
+                {}
+            )
+
+            newO.description = o.description
+
+            return {
+                ...acc,
+                [id]: newO
+            }
+        }, {})
+
+    
     fs.writeFileSync(jsonPath, JSON.stringify(newJson,null,2))
 }
