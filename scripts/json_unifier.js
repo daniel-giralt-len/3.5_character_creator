36e2048167a8c8855df8c9c8b22089b703539eb1
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-05-27 09:49:36 +0200

    fuse all rulebooks into one json

---
+++ scripts/json_unifier.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const finalJson={}
+for(let i=1; i < 108; i++){
+    const path = `../src/db/html/rulebooks/${i}.json`
+    try{   
+        finalJson[i] = require(path)
+    }catch(e){console.error('could not read', path)}
+}
+
+fs.writeFileSync('./src/db/rulebooks_data.json', JSON.stringify(finalJson, null, 2))
