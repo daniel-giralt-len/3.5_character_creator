0d1b987bd7160f47d7073f7d2a9e260a08122535
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-05-27 16:17:57 +0200

    put stats into fields

--- scripts/raceCleaner.js
+++ scripts/raceCleaner.js
@@ -4,15 +4,16 @@ const fs = require('fs')
 
 const statsRegex = new RegExp('<table class="race-attributes">(.*?)</table>')
 const descriptionRegex = new RegExp('<div class="nice-textile">(.*?)</div>')
+const jsonPath = './src/db/json/itemData/raceDescription.json'
 
-const a = 0
+const a = 1
 
 if(a===0){ //unify all htmls into one dirty json
     const json = {}
     for(let i=1; i<=42; i++){
         try{
             const path = `./src/db/html/races/${i}.html`
-            let html = fs.readFileSync(path).toString().replace(/\n\r?/g,'')
+            const html = fs.readFileSync(path).toString().replace(/\n\r?/g,'')
             json[i] = {
                 statsTable: html.match(statsRegex)[1],
                 description: html.match(descriptionRegex)[1],
@@ -20,5 +21,36 @@ if(a===0){ //unify all htmls into one dirty json
     
         }catch(e){console.error(e,i)}
     }
-    fs.writeFileSync('./src/db/json/itemData/raceDescription.json', JSON.stringify(json,null,2))
+    fs.writeFileSync(jsonPath, JSON.stringify(json,null,2))
+}
+
+if(a===1){ //clean stats
+    const json = require(`.${jsonPath}`)
+    const newJson = Object
+        .entries(json)
+        .reduce((acc, [id, {statsTable, ...rest}]) => {
+            const statRegex = RegExp(/<tr>(.*?)<\/tr>/g)
+            const statNameRegex = RegExp(/<th>(.*?):?<\/th>/)
+            const statValueRegex = RegExp(/<td>(.*?)<\/td>/)
+
+            const stats = statsTable
+                .match(statRegex)
+                .reduce((acc2,s)=>(
+                    {
+                        ...acc2,
+                        [statNameRegex.exec(s)[1]]: statValueRegex.exec(s)[1]
+                    }),
+                {})
+
+            return {
+                ...acc,
+                [id]: {
+                    ...rest,
+                    ...stats
+                }
+            }
+        },
+        {}
+    )
+    fs.writeFileSync(jsonPath, JSON.stringify(newJson,null,2))
 }
