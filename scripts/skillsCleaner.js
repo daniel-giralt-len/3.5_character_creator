c183a2ac878d2dd5415ec6bf8f04add004b35468
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-05-28 18:10:01 +0200

    add id to skills

--- scripts/skillsCleaner.js
+++ scripts/skillsCleaner.js
@@ -1,11 +1,18 @@
 const fs = require('fs')
 const csvToJson = require('./csvToJson')
 
-const a = 0;
+const a = 1;
 const inPath = './src/db/csv/skills.csv'
 const outPath = './src/db/json/skills.json'
+const dbPath = './src/db/json/dbs.json'
 
 if(a===0){
     const json = csvToJson(inPath)
     fs.writeFileSync(outPath, JSON.stringify(json,null,2))
+}
+
+if(a===1){
+    const json = require('.'+dbPath)
+    json.skills = json.skills.map((j,id) => ({id: `${(id+1)}`, ...j}))
+    fs.writeFileSync(dbPath, JSON.stringify(json,null,2))
 }
